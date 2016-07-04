9c91a66cc5816b0d537211cb07fd372042bd13e0
Thomas Le Saux <thomas.le-saux@loria.fr>
2016-07-04 16:06:45 +0200

    feat(netflux.Bot): Use of abstraction of netflux bot
    Exemple of connection with netflux-chat

--- server.js
+++ server.js
@@ -1,96 +1,56 @@
 'use strict'
-var host = '127.0.0.1'
-var port = 9000
-let WebSocketServer = require('ws').Server
-var netflux = require('netflux')
-var mongoose = require('mongoose')
-var MessageModel = require('./model/message')
 
-const ADD_BOT_SERVER = 'addBotServer'
-const NEW_CHANNEL = 'newChannel'
+let host = '127.0.0.1'
+let port = 9000
+
+process.argv.forEach((value, index, array) => {
+  if (value.match('-(h|-host)')) {
+    host = array[index + 1]
+  } else if (value.match('-(p|-port)')) {
+    port = array[index + 1]
+  }
+})
+
+let netflux = require('netflux')
+
+let mongoose = require('mongoose')
+let MessageModel = require('./model/message')
 
 mongoose.connect('mongodb://localhost/netfluxBotLog', function (err) {
   if (err) { throw err }
 })
 
-mongoose.connection.on('connected', function () {
-  let webChannels = []
+mongoose.connection.on('connected', () => {
+  let bot = new netflux.Bot()
+  bot.listen({host, port, log: true})
 
-  let server = new WebSocketServer({host, port}, () => {
-    log('WebSocketServer', 'Server runs on: ws://' + host + ':' + port)
-  })
+  bot.onWebChannel = (wc) => {
+    // For Netflux-Chat
+    let send = (msg, toIdUser) => {
+      var message = {'type': 'message', 'data': {'fromIdUser': '0', 'toIdUser': toIdUser || '0', 'content': msg,
+      'date': Date.now()}}
+      wc.send(JSON.stringify(message))
+    }
 
-  server.on('connection', (socket) => {
-    log('connected', 'Connection of one client')
+    send('Hello, I\'m a server')
 
-    socket.on('message', (msg) => {
-      var data = {code: ''}
-      try {
-        data = JSON.parse(msg)
-      } catch (e) {}
-      switch (data.code) {
-        case ADD_BOT_SERVER:
-          log('add', 'Add request received')
-          var webChannel, onJoining, onLeaving, onMessage, onChannelClose
+    wc.onJoining = (id) => {
+      let jp = new MessageModel.Message({label: 'JOIN', content: id})
+      MessageModel.saveMsg(jp).onResolve(() => {
+        send('Welcome')
+      })
+    }
 
-          onJoining = (id) => {
-            log('joining', 'Joinning of a new client [' + id + ']')
-          }
+    wc.onLeaving = (id) => {
+      let jp = new MessageModel.Message({label: 'LEAVE', content: id})
+      MessageModel.saveMsg(jp).onResolve(() => {})
+    }
 
-          onLeaving = (id) => {
-            log('leaving', 'Leaving of client [' + id + ']')
-          }
-
-          onMessage = (id, msg) => {
-            log('message', '[From ' + id + '] ' + msg)
-            var m = new MessageModel.Message({label: 'MESSAGE', content: msg, fromId: id})
-            MessageModel.saveMsg(m).onResolve(() => {
-              webChannel.send('I received your message')
-            })
-          }
-
-          onChannelClose = (evt) => {
-            // log('closed', 'WebChannel has been closed')
-          }
-
-          webChannel = new netflux.WebChannel({'connector': 'WebSocket', host, port})
-          webChannel.onJoining = onJoining
-          webChannel.onMessage = onMessage
-          webChannel.onLeaving = onLeaving
-          webChannel.onChannelClose = onChannelClose
-          webChannel.joinAsBot(socket, data.sender).then(() => {
-            webChannel.channels.forEach((value) => {
-              onJoining(value.peerId)
-            })
-            log('connected', 'Connected to the network')
-            log('id', webChannel.myId)
-            webChannel.send('Hello, I\'m a server')
-          })
-
-          webChannels.push(webChannel)
-          break
-        case NEW_CHANNEL:
-          log('new_channel', 'New channel request received')
-          // console.log('[DEBUG] wcId: ', data.wcId)
-          for (var wc of webChannels) {
-            // console.log('[DEBUG] wc.id: ', wc.id)
-            if (!data.which_connector_asked) wc.connectMeToRequests.get(data.sender)(true, socket)
-            else wc.initChannel(socket, false, data.sender)
-          }
-          break
-        default:
-          log('error', 'Unknown code message')
-      }
-    })
-  })
+    wc.onMessage = (id, msg) => {
+      let m = new MessageModel.Message({label: 'MESSAGE', content: msg})
+      MessageModel.saveMsg(m).onResolve(() => {
+        send('I received your message')
+      })
+    }
+  }
 })
-
-function getDate () {
-  var d = new Date()
-  return '' + d.toLocaleTimeString() + ' ' + d.toLocaleDateString()
-}
-
-function log (label, msg) {
-  let datetime = getDate()
-  console.log('[', label.toUpperCase(), '] [', datetime, ']', msg)
-}
