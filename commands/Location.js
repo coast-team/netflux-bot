881807d1d4f75d648fba8f7ff4a6cbb17dfa5cc6
Thomas Le Saux <thomas.le-saux@loria.fr>
2016-07-29 16:06:06 +0200

    docs(Location): Add of explanation for functions and attributes

--- commands/Location.js
+++ commands/Location.js
@@ -1,5 +1,6 @@
 var fs =  require('fs')
 
+// Check if the file netflux-bot/tokens/google.jon exists and get the api key
 if (!process.env.GOOGLE_API_KEY) {
   if (!fs.existsSync('./tokens/google.json')) {
     console.log('Create a tokens/google.json with your credentials based on the samples/google-sample.json file.')
@@ -20,7 +21,17 @@ var options = {
 
 var geocoder = NodeGeocoder(options)
 
+/**
+  * Represents a location that can be execute by the server
+  * The location is specified through the address in the settings
+  */
 class Location {
+  /**
+   * *Location* constructor. *Location* can be parameterized with the option address
+   * @param  {Object} [options] *Location* configuration.
+   * @param  {string} [options.address=''] Address of the location to be find.
+   * @returns {Location} *Location* to find.
+   */
   constructor (options = {}) {
     this.defaults = {
       address: 'Nancy'
@@ -28,10 +39,19 @@ class Location {
     this.settings = Object.assign({}, this.defaults, options)
   }
 
+  /**
+    * Find the location with the specified address.
+    * @return {Promise} It resolves once the location is find
+    * and return an Array of possible location object with latitude and longitude.
+    */
   find () {
     return geocoder.geocode(this.getAddress())
   }
 
+  /**
+    * Get the address of the location to be find.
+    * @return {string} Address of the location to be find.
+    */
   getAddress () {
     return this.settings.address
   }
