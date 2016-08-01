91479354eac057dca5419a9c9e8413053859fd95
Thomas Le Saux <thomas.le-saux@loria.fr>
2016-08-01 09:37:07 +0200

    docs(SlackStream): Add of explanation for functions and attributes

--- commands/SlackStream.js
+++ commands/SlackStream.js
@@ -1,8 +1,9 @@
 var fs =  require('fs')
 var request = require('request')
 
+// Check if the file netflux-bot/tokens/slack.jon exists and get the tokens
 if (!process.env.SLACK_ACCESS_TOKEN_RTM) {
-  if (!fs.existsSync('./tokens/twitter.json')) {
+  if (!fs.existsSync('./tokens/slack.json')) {
     console.log('Create a tokens/slack.json with your credentials based on the samples/slack-sample.json file.')
     process.exit(1)
   } else {
@@ -14,7 +15,21 @@ if (!process.env.SLACK_ACCESS_TOKEN_RTM) {
 
 var Botkit = require('botkit')
 
+/**
+  * Represents a connection with slack
+  */
 class SlackStream {
+  /**
+   * *SlackStream* constructor. *SlackStream* can be parameterized
+   * with options like channel, users, ...
+   * @param  {Object} [options] *SlackStream* configuration.
+   * @param  {string} [options.message=''] Message to send to slack through the bot.
+   * @param  {string} [options.channel='#test'] Channel on which the messages will be listen and send.
+   * @param  {Object} [options.users=null] Map of users (Peer id => name) for the slack connection.
+   * @param  {Object} [options.send=null] Send function of the bot server.
+   * @param  {number} [options.id=0] Id of the peer who send the command.
+   * @returns {SlackStream} *SlackStream* to launch.
+   */
   constructor (options = {}) {
     this.defaults = {
       message: '',
@@ -23,7 +38,16 @@ class SlackStream {
       send: null,
       id: 0
     }
+
+    /**
+      * Controller used to get message from slack.
+      */
     this.controller = Botkit.slackbot()
+
+    /**
+      * Bot entity for slack side.
+      * Used to send message to slack.
+      */
     this.bot = this.controller.spawn({
       token: process.env.SLACK_ACCESS_TOKEN_RTM,
       incoming_webhook: {
@@ -33,6 +57,10 @@ class SlackStream {
     this.settings = Object.assign({}, this.defaults, options)
   }
 
+  /**
+    * Start a stream connection with slack.
+    * @return {Promise} It resolves once the connection is established
+    */
   startRTM () {
     return new Promise((resolve, reject) => {
       this.bot.startRTM((err, bot, payload) => {
@@ -42,11 +70,17 @@ class SlackStream {
     })
   }
 
+  /**
+    * Begin the connection with slack and listen messages from it.
+    * @return {Promise} It resolves once the connection is established
+    * and a "Hello I'm a bot" is send.
+    */
   launch () {
     return new Promise((resolve, reject) => {
       this.startRTM()
         .then(() => this.sendToSlack())
         .then(() => resolve())
+      // Listen all messages from slack and send them to the WebChannel.
       this.controller.on('ambient', (bot, message) => {
         this.getUserInfos(message.user)
           .then((data) => {
@@ -56,10 +90,19 @@ class SlackStream {
     })
   }
 
+  /**
+    * Close the connection with slack.
+    */
   close () {
     this.bot.closeRTM()
   }
 
+  /**
+    * Get user informations to a user of slack.
+    * @param {number} Slack id of a user.
+    * @return {Promise} It resolves once the informations of the user are received
+    * and return an object representing the user.
+    */
   getUserInfos (userId) {
     return new Promise((resolve, reject) => {
       let url = 'https://slack.com/api/users.info?token=' + process.env.SLACK_ACCESS_TOKEN_WEB +
@@ -71,6 +114,16 @@ class SlackStream {
     })
   }
 
+  /**
+   * Send messages to slack with options like channel, users, ...
+   * @param  {Object} [options] *SlackStream* configuration.
+   * @param  {string} [options.message=''] Message to send to slack through the bot.
+   * @param  {string} [options.channel='#test'] Channel on which the messages will be listen and send.
+   * @param  {Object} [options.users=null] Map of users (Peer id => name) for the slack connection.
+   * @param  {Object} [options.send=null] Send function of the bot server.
+   * @param  {number} [options.id=0] Id of the peer who send the command.
+   * @return {Promise} It resolves once the message sent.
+   */
   sendToSlack (options = {}) {
     this.settings = Object.assign({}, this.settings, options)
     let user = this.getUsers().get(this.getId()) || this.getId()
@@ -86,30 +139,57 @@ class SlackStream {
     })
   }
 
+  /**
+    * Get the message to send to slack through the bot.
+    * @return {string} Message to send to slack through the bot.
+    */
   getMessage () {
     return this.settings.message
   }
 
+  /**
+    * Set the message to send to slack through the bot.
+    * @param {string} Message to send to slack through the bot.
+    */
   setMessage (message) {
     this.settings.message = message
   }
 
+  /**
+    * Get the channel on which the messages will be listen and send.
+    * @return {string} Channel on which the messages will be listen and send.
+    */
   getChannel () {
     return this.settings.channel
   }
 
+  /**
+    * Set the channel on which the messages will be listen and send.
+    * @param {string} Channel on which the messages will be listen and send.
+    */
   setChannel (channel) {
     this.settings.channel = channel
   }
 
+  /**
+    * Get the map of users (Peer id => name) for the slack connection.
+    * @return {Object} Map of users (Peer id => name) for the slack connection.
+    */
   getUsers () {
     return this.settings.users
   }
 
+  /**
+    * Get the id of the peer who send the command.
+    * @return {number} Id of the peer who send the command.
+    */
   getId () {
     return this.settings.id
   }
 
+  /**
+    * Alias of the send method of the bot server.
+    */
   send (str) {
     this.settings.send(str)
   }
