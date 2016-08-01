930fa7a146c64850405550029150ac4601981054
Thomas Le Saux <thomas.le-saux@loria.fr>
2016-08-01 15:46:17 +0200

    docs(Readme): Add of explanations for commands

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Netflux-bot
-Peer machine able to connect to a network handled by netflux API
+Peer machine able to connect to a network handled by [Netflux API](https://github.com/coast-team/netflux)
 
 ----------
 # Install & Run Netflux-bot on Raspberry Pi
@@ -11,7 +11,7 @@ Tested on
 In this guide we install the following appications:
 
  - NodeJS: 6.2.2
- - Netlfux: latest
+ - Netflux: latest
 
 ----------
 ## Install Netlfux-bot
@@ -39,9 +39,13 @@ Navigate to `netflux-bot` folder and run the server.
 
 By default the server listen on `ws://127.0.0.1:9000`
 
-Howover you can specify an host and a port with the options `(-h|--host & -p|--port)`:
+Howover you can specify an host and a port with the options `(-h|--host) HOST` and `(-p|--port) PORT`:
 
     node server.js -h 192.168.0.2 -p 8080
+
+There are also other options:
+  - `(-nc|--netfluxChat) BOOLEAN`: Enable/Disable Netflux-chat protocol for the messages
+  - `(-s|--save) BOOLEAN`: Enable/Disable default saving status of the bot
 
 ### On system boot
 To run Netflux-bot on system boot with help of **Forever** node package. It ensure that Netflux-bot application will run continuously.
@@ -73,7 +77,7 @@ case "$1" in
    start)
     echo "Starting Netflux Bot"
     # run application you want to start
-    /home/pi/.nvm/versions/node/v6.2.2/bin/node /home/pi/.nvm/versions/node/v6.2.2/bin/forever --workingDir /home/pi/netflux-bot/ /home/pi/netflux-bot/server.js -nc false -h [RASPBERRY_PI_IP_ADDRESS] -p [RASPBERRY_PI_PORT] >> /home/pi/netflux-bot/server.log
+    /home/pi/.nvm/versions/node/v6.2.2/bin/node /home/pi/.nvm/versions/node/v6.2.2/bin/forever --workingDir /home/pi/netflux-bot/ /home/pi/netflux-bot/server.js -h [RASPBERRY_PI_IP_ADDRESS] -p [RASPBERRY_PI_PORT] >> /home/pi/netflux-bot/server.log
    ;;
    stop)
     echo "Stopping Netflux Bot"
@@ -103,4 +107,32 @@ Enable dependency-base boot sequencing:
 
 ----------
 ## Commands
-`// TODO`
+
+Once you have launched the server and you have added him in [Netflux Chat](https://github.com/coast-team/netflux-chat) by example, you can communicate with him through commands.
+
+A list of the available commands can be find with the execution of the command `/help` in the WebChannel where the bot has been added.
+
+This list is the same as the following one:
+  - `/server`: Command to verify the status of the bot server
+  - `/save`: Command to start/stop saving message in a database
+  - `/kick`: Command to kick the bot server from the WebChannel
+  - `/twitter`: Command to stream messages from Twitter
+  - `/location`: Command to get coordinates of an address
+  - `/slack`: Command to stream and send message from and to Slack
+  - `/help`: Command that displays this help
+
+Each commands comes with a help message than can be get by an argument:
+
+    /COMMAND_NAME -h
+
+or
+
+    /COMMAND_NAME --help
+
+By example for the commands `/twitter -h` or `/twitter --help` the help message will be:
+
+    Help for the command {/twitter}:
+    {/twitter -u USER} or {/twitter --user USER} Command to stream all messages post by {USER}
+    {/twitter -q QUERY} or {/twitter --query QUERY} Command to stream all messages which contains {QUERY}
+    {/twitter -s} or {/twitter --stop} Command to stop streaming all messages
+    {/twitter -h} or {/twitter --help} Display this message
--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 'use strict'
 
 let Command = require('./commands/Command').Command
+let netflux = require('netflux')
+let MessageModel = require('./model/message')
 
+// Configuration of the default WebSocketServer
 let host = '127.0.0.1'
 let port = 9000
+
+// Saving status of the bot server
 let save = false
+
+// Protocol of the messages
 let netfluxChat = true
+
+// Streams objects for twitter and slack connections
 let twitterStream = null
 let slackStream = null
-let slack = false
+
+// Map of users (Peer id => name) for the slack connection
 let users = new Map()
 
+// Constants for the database storing
 const MESSAGE = 'MESSAGE'
 const COMMAND = 'COMMAND'
 const JOIN = 'JOIN'
 const LEAVE = 'LEAVE'
 
+// Check arguments to configure the bot server
 process.argv.forEach((value, index, array) => {
   if (value.match('-(h|-host)')) {
     host = array[index + 1]
@@ -29,9 +41,7 @@ process.argv.forEach((value, index, array) => {
   }
 })
 
-let netflux = require('netflux')
-let MessageModel = require('./model/message')
-
+// Function to launch the bot server
 let init = () => {
   let bot = new netflux.Bot()
   bot.listen({host, port, log: true})
@@ -39,16 +49,23 @@ let init = () => {
       console.log('Bot listen on ws://' + bot.settings.host + ':' + bot.settings.port)
     })
 
+  // Once the bot is added to a WebChannel this function is called
   bot.onWebChannel = (wc) => {
     let id = wc.myId
 
-    let send = (msg, toIdUser) => {
+    /**
+      * Send the message to the WebChannel
+      * @param {string} msg - Message to be send
+      * @param {number} toIdUser - Id of the recipient peer (default = 0)
+      */
+    let send = (msg, toIdUser = 0) => {
       if (netfluxChat) {
+        // Protocol of Netflux-chat messages
         var message = {
           'type': 'message',
           'data': {
             'fromIdUser': id,
-            'toIdUser': toIdUser || '0',
+            'toIdUser': '' + toIdUser,
             'content': msg,
             'date': Date.now()
           }
@@ -58,6 +75,9 @@ let init = () => {
       if (save) MessageModel.saveMsg(new MessageModel.Message({label: MESSAGE, content: msg, fromId: id}))
     }
 
+    /**
+      * Send a hello world message and user informations for Netflux-chat
+      */
     let hello = () => {
       if (netfluxChat) {
         var userInfos = {
@@ -78,15 +98,11 @@ let init = () => {
       send('Hello, I\'m a server')
     }
 
-    let getUserId = (user) => {
-      return new Promise((resolve, reject) => {
-        client.get('users/show', {screen_name: user}, (error, user, response) => {
-          if (error) reject()
-          else resolve(user.id)
-        })
-      })
-    }
-
+    /**
+      * Analyse a message to find commands to execute
+      * @param {string} str - Message to analyse
+      * @param {number} fromId - Id of the peer who sent the message
+      */
     let analyse = (str, fromId) => {
       let label = MESSAGE
       let regexCommands = /\/(\w+)\s*((?:(?:([\w\d]|\#)+|\"([^"]|\')+\"|-\w+)\s*)*)/g
@@ -118,7 +134,7 @@ let init = () => {
       }
     }
 
-
+    // If someone join the WebChannel this function is called
     wc.onJoining = (id) => {
       if (save) {
         MessageModel.saveMsg(new MessageModel.Message({label: JOIN, content: id}))
@@ -127,10 +143,12 @@ let init = () => {
       else send('Welcome')
     }
 
+    // If someone leave the WebChannel this function is called
     wc.onLeaving = (id) => {
       if (save) MessageModel.saveMsg(new MessageModel.Message({label: LEAVE, content: id}))
     }
 
+    // If someone send a message to the WebChannel this function is called
     wc.onMessage = (id, msg) => {
       let content
       if (netfluxChat) content = JSON.parse(msg).data.content
